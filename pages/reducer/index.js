cae8fd0ab0e826f6150cd22c8b580d089d1695ef
Arin Paliwal <paliwalarin09@gmail.com>
2024-07-02 20:55:40 +0530

    Added Reducer & Class Component

---
+++ pages/reducer/index.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increment = () => dispatch({ type: 'increment' });
+  const decrement = () => dispatch({ type: 'decrement' });
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+//Dispatch Function: A function that allows you to trigger state updates by dispatching actions
+// Current State: The current state of your component, based on the initial state and any updates.
